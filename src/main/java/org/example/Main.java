349a62d6f5ea841777d9f65318f465c5d017f256
Adrian030304 <adrianromanenco1@gmail.com>
2025-11-24 02:24:18 +0200

    handle collection as stream

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,19 +6,19 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-//        Printer printer = new Printer();
-//        printer.printManyTimes(new Clasa1("AA"), 5);
-//        CharSequence sirCaractere = "ADRAIN";
-//        printer.printCharacters(sirCaractere);
-//        Object ob = new Clasa1("BB");
-//        System.out.println(ob.hashCode());
-//        System.out.println(ob);
+        Scanner scanner = new Scanner(System.in);
+        List<String> inputs = new ArrayList<>();
 
-        Movable muta = new Organism(10,12);
-        System.out.println(muta);
-        muta.move(2,3);
-        System.out.println(muta);
+        while (true) {
+            String row = scanner.nextLine();
+            if (row.equals("end")) break;
+            inputs.add(row);
+        }
 
+        long numbersDivisible = inputs.stream()
+                .mapToInt(s -> Integer.valueOf(s))
+                .filter(number -> number % 3 == 0)
+                .count();
 
     }
 }
